70ab7e2131da112cbaf3de38cd8b3c2db0b1b7c2
freergit <fredrik-98@live.se>
2025-03-28 09:32:27 +0100

    remove VC creation

--- issuer/src/App.tsx
+++ issuer/src/App.tsx
@@ -1,49 +1,13 @@
 import { useState, useEffect } from "react";
 import { QRCodeSVG } from "qrcode.react";
 
-import { createVerifiableCredential } from "vc-js";
-import * as ed from '@noble/ed25519';
 
-// const documentLoader = securityLoader().build();
 
 // Function to generate a UUID (v4)
 const generateNonce = () => crypto.randomUUID();
 
 async function App() {
   const nonce = generateNonce();
-
-
-  const vc = {
-    '@context': ['https://www.w3.org/2018/credentials/v1'],
-    type: ['VerifiableCredential'],
-    issuer: 'did:example:issuer123',
-    issuanceDate: new Date().toISOString(),
-    credentialSubject: {
-      id: 'did:example:john_doe',
-      name: 'John Doe',
-      email: 'john.doe@example.com'
-    }
-  };
-
-  const privateKey = ed.utils.randomPrivateKey(); // Generate a random private key (32 bytes)
-  const publicKey = await ed.getPublicKey(privateKey); // Get the corresponding public key
-  const suite = {
-    type: 'Ed25519Signature2018',
-    sign: async (data: Buffer) => {
-      return ed.sign(data, privateKey); // Sign the data (VC) with the private key
-    },
-    publicKey: publicKey // Include the public key
-  };
-
-  const signedVC = await createVerifiableCredential({
-    credential: vc,
-    suite, // Use the signature suite created above
-    documentLoader: () => {
-      return { contextUrl: null, document: vc };
-    }
-  });
-  console.log("Signed VC:", signedVC);
-
 
   useEffect(() => {
     const socket = new WebSocket("ws://52.158.36.185:8000");
@@ -53,21 +17,7 @@ async function App() {
     };
 
     socket.onmessage = async (event) => {
-
-
-      // const keyPair = await Ed25519KeyPair.generate();
-
-      // // Sign the VC using the key pair
-      // const signedVC = await createVerifiableCredential({
-      //   credential: vc,
-      //   suite: keyPair, // Sign the VC with the key pair
-      //   documentLoader: () => {
-      //     // Provide a document loader for resolving contexts (e.g., for JSON-LD)
-      //     return { contextUrl: null, document: vc };
-      //   }
-      // });
-
-      // console.log(signedVC);
+      console.log(event.data)
     };
 
 
