bebf0b367cbef9f4eb32fb64215705dd492a83ed
freergit <fredrik-98@live.se>
2025-04-08 14:09:18 +0200

    use resolvable DID for issuer

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Lorum ipsum etc
 
 ## ./scripts
 Simple jobs such as submitting a DID document to solana devnet and updating it, see [README.md](./scripts/README.md)
+
+
+## Current DIDs
+issuer: did:sol:devnet:7e3bAN1vRNL7c73awhtwJpXmun5YwhCQffieGoi8vdsb
+
+wallet: did:sol:devnet:E13rUS8is8BU2J7KYnnti4rrmZ6hFCKW6JF9F33QCKhT
+
+## Considerations / Future work / TODOs
+**Issue:** 
+A issuer may use many variations of suites (Ed25519-2018, Ed25519-2020, jws-2020, x25519-2019, and so on..), currently we use one suite over the entire architecture. How should one handle N suites?
+
+**Issue:**
+Currently the Issuer uses the same keypair for the Solana account (DID) and for the signature itself for VCs. They should ideally be seperate, both for generality but also for the ability to do key rotations and key revocation.
+
--- issuer/server/src/server.ts
+++ issuer/server/src/server.ts
@@ -2,30 +2,29 @@ import express from 'express';
 import { WebSocketServer } from 'ws';
 import http from 'http';
 import * as vcIssuer from "@digitalbazaar/vc"
-import * as verKey from "@digitalbazaar/ed25519-verification-key-2020"
-import * as sig from "@digitalbazaar/ed25519-signature-2020"
+import { Ed25519VerificationKey2018 } from '@digitalbazaar/ed25519-verification-key-2018';
+import { Ed25519Signature2018 } from '@digitalbazaar/ed25519-signature-2018';
 import jsonld from "jsonld"
-import { Ed25519Signature2020 } from
-    '@digitalbazaar/ed25519-signature-2020';
 
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const credential = JSON.parse(readFileSync(join(__dirname, "../../data/credential.json"), "utf-8"));
-const credentialExample = JSON.parse(readFileSync(join(__dirname, "../../data/credentialExample.json"), "utf-8"));
-const issuerExample = JSON.parse(readFileSync(join(__dirname, "../../data/issuerExample.json"), "utf-8"));
+// const credential = JSON.parse(readFileSync(join(__dirname, "../../data/credential.json"), "utf-8"));
+// const credentialExample = JSON.parse(readFileSync(join(__dirname, "../../data/credentialExample.json"), "utf-8"));
+// const issuerExample = JSON.parse(readFileSync(join(__dirname, "../../data/issuerExample.json"), "utf-8"));
 const securitySuites = JSON.parse(readFileSync(join(__dirname, "../../data/securitySuites.json"), "utf-8"));
-const jsonLd = JSON.parse(readFileSync(join(__dirname, "../../data/jsonLd.json"), "utf-8"));
+// const jsonLd = JSON.parse(readFileSync(join(__dirname, "../../data/jsonLd.json"), "utf-8"));
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// const wss = new WebSocketServer({ noServer: true });
+const wss = new WebSocketServer({ noServer: true });
 
 // client connects, sends unique nonce
 // Wallet scans qr code to attain the nonce
@@ -33,25 +32,30 @@ const jsonLd = JSON.parse(readFileSync(join(__dirname, "../../data/jsonLd.json")
 // Lookup in the map to find the associated connection to the nonce
 // Push the data acquired from the wallet
 const nonceToConnectionMap = new Map();
-// TODO: remove the entry on close (probably wont do this though lmeow)
-
-const keypair = await verKey.Ed25519VerificationKey2020.generate();
+// TODO: remove the entry on close (probably wont do this though)
 
 const customContexts: Record<string, object> = {
-    "https://www.w3.org/2018/credentials/v1": credential,
-    "https://www.w3.org/2018/credentials/examples/v1": credentialExample,
-    "http://example.com/": issuerExample,
+    // "https://www.w3.org/2018/credentials/v1": credential,
+    // "https://www.w3.org/2018/credentials/examples/v1": credentialExample,
+    // "http://example.com/": issuerExample,
     "https://w3id.org/security/suites/ed25519-2020/v1": securitySuites,
-    "https://www.w3.org/ns/odrl.jsonld": jsonLd
+    // "https://www.w3.org/ns/odrl.jsonld": jsonLd
 };
 
-const key = await verKey.Ed25519VerificationKey2020.generate({
-    id: "did:example:issuer123#1",
-    controller: "did:example:issuer123",
-    // publicKeyMultibase: `z6MkrTp3mSzGM8qisEUqPhvjsVPhsCWvMSNq7J6cbsmgT6dM`
+
+const parsed = dotenv.config().parsed!;
+
+const issuer_did = "did:sol:devnet:7e3bAN1vRNL7c73awhtwJpXmun5YwhCQffieGoi8vdsb";
+
+const key = await Ed25519VerificationKey2018.generate({
+    id: "did:sol:devnet:7e3bAN1vRNL7c73awhtwJpXmun5YwhCQffieGoi8vdsb#default",
+    type: "Ed25519VerificationKey2018",
+    publicKeyBase58: parsed["PUBLIC_KEY"],
+    privateKeyBase58: parsed["PRIVATE_KEY"]
 });
 
-const suite = new Ed25519Signature2020({ key });
+const suite = new Ed25519Signature2018({ key });
+
 
 const customDocumentLoader = async (url: string) => {
     if (customContexts[url]) {
@@ -65,87 +69,82 @@ const customDocumentLoader = async (url: string) => {
     return jsonld.documentLoaders.node()(url);
 }
 
-const generateVC = async (keyPair, did) => {
-    keyPair.id = "did:example:issuer123#1";
-    keyPair.controller = "did:example:issuer123";
-    const suite = new sig.Ed25519Signature2020({ key: keyPair });
+const generateVC = async (issuer, wallet) => {
+    // keyPair.id = "did:example:issuer123#1";
+    // keyPair.controller = "did:example:issuer123";
+    // const suite = new Ed25519Signature2018({ key: keyPair });
     const credential = {
         "@context": [
             "https://www.w3.org/2018/credentials/v1",
-            "https://www.w3.org/2018/credentials/examples/v1"
+
+            // "https://www.w3.org/2018/credentials/examples/v1"
         ],
-        "type": ["VerifiableCredential", "AlumniCredential"],
-        "issuer": "http://example.com/",
+        "type": ["VerifiableCredential"],
+        "issuer": issuer,
         "issuanceDate": "2010-01-01T19:23:24Z",
         "credentialSubject": {
-            "id": did,
-            "alumniOf": "Example University"
+            "id": wallet,
+            // "alumniOf": "Example University"
         }
     };
-
-    const signedVC = await vcIssuer.issue({ credential, suite, documentLoader: customDocumentLoader });
+    // , documentLoader: customDocumentLoader }
+    const signedVC = await vcIssuer.issue({ credential, suite });
     return signedVC;
 }
-const did = "did:sol:devnet:EjReiZwCkicEGBuBQsf3qaK7DSX8hio5whzhTeExYYKw"
-const vc = await generateVC(keypair, did)
 
-console.log(vc)
 
-const presentation = vcIssuer.createPresentation({
-    verifiableCredential: [vc]
+// const presentation = vcIssuer.createPresentation({
+//     verifiableCredential: [vc]
+// });
+
+
+// const vp = await vcIssuer.signPresentation({
+//     presentation, suite, challenge: "idk", documentLoader: customDocumentLoader
+// })
+
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+
+    ws.on("message", (message) => {
+        const { nonce } = JSON.parse(message.toString());
+        console.log("Received nonce:", nonce);
+
+        nonceToConnectionMap.set(nonce, ws);
+
+        ws.send(JSON.stringify({ message: "Nonce received successfully!" }));
+    });
+
+    ws.on("close", () => {
+        nonceToConnectionMap.clear();
+        console.log("Client disconnected");
+    });
 });
 
-console.log(presentation)
+app.post('/present-did', async (req, res) => {
+    const { nonce, did, data } = req.body; // did -> wallet did
+    console.log(`Received nonce via HTTP: ${nonce}`);
+    if (nonceToConnectionMap.has(nonce)) {
+        const ws = nonceToConnectionMap.get(nonce);
+        ws.send(JSON.stringify({ message: "Data from server", data }))
+        const vc = await generateVC(issuer_did, did)
+        const response = {
+            title: "A VC shared by QR code",
+            vc: vc,
+        };
+        res.status(200).send(JSON.stringify(response));
+    } else {
+        res.status(404).send('Nonce not found!');
+    }
+});
 
-const vp = await vcIssuer.signPresentation({
-    presentation, suite, challenge: "idk", documentLoader: customDocumentLoader
-})
+const server = http.createServer(app);
 
-console.log(vp)
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
 
-// wss.on("connection", (ws) => {
-//     console.log("Client connected");
-
-//     ws.on("message", (message) => {
-//         const { nonce } = JSON.parse(message.toString());
-//         console.log("Received nonce:", nonce);
-
-//         nonceToConnectionMap.set(nonce, ws);
-
-//         ws.send(JSON.stringify({ message: "Nonce received successfully!" }));
-//     });
-
-//     ws.on("close", () => {
-//         nonceToConnectionMap.clear();
-//         console.log("Client disconnected");
-//     });
-// });
-
-// app.post('/present-did', async (req, res) => {
-//     const { nonce, did, data } = req.body;
-//     console.log(`Received nonce via HTTP: ${nonce}`);
-//     if (nonceToConnectionMap.has(nonce)) {
-//         const ws = nonceToConnectionMap.get(nonce);
-//         ws.send(JSON.stringify({ message: "Data from server", data }))
-//         const vc = await generateVC(keypair, did)
-//         const response = {
-//             title: "A VC shared by QR code",
-//             vc: vc,
-//         };
-//         res.status(200).send(JSON.stringify(response));
-//     } else {
-//         res.status(404).send('Nonce not found!');
-//     }
-// });
-
-// const server = http.createServer(app);
-
-// server.on('upgrade', (request, socket, head) => {
-//     wss.handleUpgrade(request, socket, head, (ws) => {
-//         wss.emit('connection', ws, request);
-//     });
-// });
-
-// server.listen(8000, () => {
-//     console.log("HTTP and WebSocket server is running on http://localhost:8000");
-// });
+server.listen(8000, () => {
+    console.log("HTTP and WebSocket server is running on http://localhost:8000");
+});
